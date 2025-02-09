f0cdf3f4f936fdfdf6bc3839ed8e1db0fbfd4008
Ashwin Prabou <aprabou@ucsc.edu>
2025-02-09 04:33:41 -0800

    init2

---
+++ src/landing.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./landing.css";
+
+const Landing: React.FC = () => {
+  const navigate = useNavigate();
+
+  const sen1 = [
+    { text: "It's", className: "white" },
+    { text: " hard ", className: "yellow" },
+    { text: "to hold ourselves accountable.", className: "white" },
+  ];
+  const sen2 = [
+    { text: "That's why you have Leet", className: "white" },
+    { text: "Streak", className: "yellow" },
+    { text: ".", className: "white" },
+  ];
+  const sen3 = [
+    { text: "To", className: "white" },
+    { text: " ignite ", className: "yellow" },
+    { text: "that motivation and better your peers.", className: "white" },
+  ];
+
+  return (
+    <div>
+      <h1>
+        <span className="white">Leet</span>
+        <span className="yellow">Streak</span>
+        <button className="login" onClick={() => navigate("/getstarted.tsx")}>
+          Log In
+        </button>
+
+        <button className="signup" onClick={() => navigate("/getstarted.tsx")}>
+          Sign Up
+        </button>
+      </h1>
+      <img src="gray_square.png" align="right"></img>
+      <h2>
+        {sen1.map((word, index) => (
+          <span key={index} className={word.className}>
+            {word.text}
+          </span>
+        ))}
+        <br></br>
+        <br></br>
+        {sen2.map((word, index) => (
+          <span key={index} className={word.className}>
+            {word.text}
+          </span>
+        ))}
+        <br></br>
+        <br></br>
+        {sen3.map((word, index) => (
+          <span key={index} className={word.className}>
+            {word.text}
+          </span>
+        ))}
+        <br></br>
+      </h2>
+      <button
+        className="getStartedBig"
+        onClick={() => navigate("/getstarted.tsx")}
+      >
+        Get Started!
+      </button>
+    </div>
+  );
+};
+
+export default Landing;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import Landing from "./landing.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <BrowserRouter>
+      <Landing />
+    </BrowserRouter>
+  );
+} else {
+  console.error("Root element. is NULL");
+}
